3d988aa68e35f268ed6fd9147ba94025b2c2fd85
Stas Svasenko <stasiksvasenko3@gmail.com>
2024-10-27 19:55:13 +0200

    ptact 3

---
+++ practicals/practicals/Практична 1.4.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "uk_UA");
+    cout << "Завдання 2" << endl;
+    int N;
+    cout << "Введiть N: ";
+    cin >> N;
+
+    int sum = 0, count = 0; 
+
+    for (int i = 0; i <= N; i++) {
+        if (i % 2 != 0) {  
+            sum += i;    
+            count++;    
+        }
+    }
+
+    if (count > 0) {
+        double average = sum / count;
+        cout << "Середнє арифметичне непарних чисел: " << average << endl;
+    }
+    else {
+        cout << "Немає непарних чисел на цьому промiжку." << endl;
+    }
+    cout << "Завдання 3" << endl;
+        double b1, q;
+        int n;
+        cout << "Введiть перший член прогресiї (b1): ";
+        cin >> b1;
+        cout << "Введiть знаменник прогресiї (q): ";
+        cin >> q;
+        cout << "Введiть кiлькiсть елементiв (n): ";
+        cin >> n;
+
+        double sum1 = 0;
+
+        for (int i = 0; i < n; ++i) {
+            sum1 += b1 * pow(q, i);
+        }
+
+        double average = sum1 / n;
+
+        cout << "Середнє арифметичне перших " << n << " елементiв: " << average << endl;
+
+        return 0;
+    }
---
+++ practicals/practicals/практична 3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+// завдання 2
+int main()
+{
+	setlocale(LC_ALL, "uk_UA");
+	std::cout << "Завдання 2" << std::endl;
+	int A, B;
+	std::cout << "Введiть значення А: ";
+	std::cin >> A;
+	std::cout << "Введiть значення B: ";
+	std::cin >> B;
+	if (A * B < 0)
+	{
+		int C = (A * B) * 2;
+		std::cout << "Результат: " << C;
+	}
+	else if (A * B > 0)
+	{
+		int C = (A * B) * 1.5;
+		std ::cout << "Результат: " << C << std::endl;
+	}
+	// завдання 3
+	std::cout << "Завдання 3" << std::endl;
+	int a, b, c;
+	std::cout << "Введiть значення a: ";
+	std::cin >> a;
+	std::cout << "Введiть значення b: ";
+	std::cin >> b;
+	std::cout << "Введiть значення c: ";
+	std::cin >> c;
+	if (a < b + c && b < a + c && c < a + b) {
+		std::cout << "Можуть ";
+	}
+	else
+		std::cout << "Не можуть ";
+	return 0;
+
+}
