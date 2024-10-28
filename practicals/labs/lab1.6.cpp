0d1229357fae406482f454ef3f8f16605fa6b34e
stasik007-ui <stasiksvasenko3@gmail.com>
2024-10-28 10:44:19 +0200

    Update lab1.6.cpp

--- practicals/labs/lab1.6.cpp
+++ practicals/labs/lab1.6.cpp
@@ -1,42 +1,35 @@
-// Священко Станіслав Володимирович 123 група лабораторна 1.6
+
 #include <iostream>
 int main() {
-	const char CONST1 = '<'; // Оголошення константи та її опис
-	char Var1 = '7'; // змінної та її опис
-	char Var2; // Оголошення змінної
-	Var2 = 'R'; // ЇЇ опис
+	const char CONST1 = '<'; 
+	char Var1 = '7'; 
+	char Var2; 
+	Var2 = 'R'; 
 
-	const char CONST2 = 0x1a; // Оголошення константи 
+	const char CONST2 = 0x1a; 
 	char Var3 = 0x32;
 	char Var4 = 0x63;
 
-	// Опис змінних типу int, float, unsigned short
 	int A;
 	float B;
 	unsigned short C;
 
-	// Ініціювання змінних, що описані в
 	A = 682;
 	B = -8.4e2;
 	C = 25921;
 
-	// Опис змінних типів double, int, char
 	double dblA;
 	int nB;
 	char chC;
-
-    // Ініціювання змінних
-	// неявне приведення типів
+	
 	dblA = A;
 	nB = B;
 	chC = C;
 
-	// явне
 	dblA = double(A);
 	nB = (int)B;
 	chC = char(C);
 
-	// з обходом суворої типізації
 	double* pdblA;
 	void* pVoi;
 	pVoi = &A;
